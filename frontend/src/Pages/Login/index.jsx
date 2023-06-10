e1094a4c5af6d6fa89b80d849b2e538152b4a953
Ibnul Ashir <ibnulashir42@gmail.com>
2023-06-10 22:09:32 +0600

    login page design

--- frontend/src/Pages/Login/index.jsx
+++ frontend/src/Pages/Login/index.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux"
 import { useLocation, useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
 import { login } from "../../features/auth/authSlice"
+import { HiOutlineMail } from 'react-icons/hi'
+import { FaLock } from 'react-icons/fa'
 
 const Login = () => {
   const location = useLocation()
@@ -52,48 +54,71 @@ const Login = () => {
   return (
     <div className="flex flex-col py-2 justify-center items-center">
       <form
-        onSubmit={onSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 my-10 max-w-lg w-full mx-auto">
-          <p className="text-center text-xl font-bold my-5">Login to Cryptooescrow.com</p>
+        onSubmit={onSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 my-10 max-w-3xl w-full mx-auto">
+        <p className="text-center text-xl font-bold my-5">Login to Cryptooescrow.com</p>
         <div className="mb-4">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-gray-700 text-sm font-medium mb-2 capitalize"
             htmlFor="username"
           >
-            Email
+            Please enter your email Address <span className="text-red-600">*</span>
           </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            name="email"
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={onChange}
-          />
+
+          <div className="relative">
+            <input
+              className=" appearance-none border border-gray-200 rounded w-full py-2 pl-10 pr-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              name="email"
+              type="text"
+              // placeholder="Email"
+              value={email}
+              onChange={onChange}
+            />
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <HiOutlineMail className="w-5 h-5 text-lime-600" />
+              <div className="ml-1 h-6 w-[1px] bg-gray-200"></div>
+            </div>
+          </div>
+
         </div>
         <div className="mb-6">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-gray-700 text-sm font-medium mb-2 capitalize"
             htmlFor="password"
           >
-            Password
+            please enter your password <span className="text-red-600">*</span>
           </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            name="password"
-            type="password"
-            placeholder="******************"
-            value={password}
-            onChange={onChange}
-          />
+          <div className="relative">
+            <input
+              className=" appearance-none border border-gray-200 rounded w-full py-2 pl-10 pr-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              type="password"
+              // placeholder="******************"
+              value={password}
+              onChange={onChange}
+            />
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <FaLock className="w-5 h-5 text-lime-600" />
+              <div className="ml-1 h-6 w-[1px] bg-gray-200"></div>
+            </div>
+          </div>
         </div>
-        <div className="flex items-center justify-center">
+        <div className="flex flex-col gap-5 items-center justify-center">
           <button
-            className="bg-primary hover:bg-hover text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-primary hover:bg-hover text-white bg-lime-600 w-full font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline uppercase"
             type="submit"
           >
-            Sign In
+            log in
+          </button>
+
+          <p className="uppercase text-center">- or -</p>
+
+          <button
+            className="bg-primary hover:bg-hover text-lime-600 border border-lime-600 w-full font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Register an Account
           </button>
           {/* <a
             className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
