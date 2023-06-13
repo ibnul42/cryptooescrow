d0ebeb892229b9f51ccc6ebd017f0ac548d493bc
Ibnul Ashir <ibnulashir42@gmail.com>
2023-06-13 12:23:10 +0600

    changes

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "../features/auth/authSlice"
-import assetReducer from "../features/asset/assetSlice"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    asset: assetReducer,
   },
 })
